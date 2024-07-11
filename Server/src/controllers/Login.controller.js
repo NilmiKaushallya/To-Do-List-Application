e7baed735458996a8308feb3f303fb4415ce2c35
Nilmi Kaushallya Lakshani <nilmikaushallya@gmail.com>
2024-07-11 10:15:29 +0530

    Bug fixed in registering component and completed the rest of Login component

--- Server/src/controllers/Login.controller.js
+++ Server/src/controllers/Login.controller.js
@@ -1,7 +1,7 @@
 import { validationResult } from "express-validator";
 import User from "../models/User.js";
 import { jsonGenerate } from "../utils/helpers.js";
-import { StatusCode } from "../utils/constants.js";
+import { JWT_TOKEN_SECRET, StatusCode } from "../utils/constants.js";
 import bcrypt from "bcrypt";
 import { json } from "express";
 import Jwt from "jsonwebtoken";
@@ -29,11 +29,9 @@ const Login = async (req, res)=> {
         );
       }
 
-      const token=Jwt.sign({userId:user._id},JWT_TOKEN_SECRET);
+      const token=Jwt.sign({userId:user._id}, JWT_TOKEN_SECRET);
 
       return res.json(jsonGenerate(StatusCode.SUCCESS,"Login Successful",{userId:user._id,token:token}))
-
-
     }
 
     res.json(jsonGenerate(StatusCode.VALIDATION_ERROR,"validation error", errors.mapped()));
--- Server/src/controllers/Register.controller.js
+++ Server/src/controllers/Register.controller.js
@@ -5,47 +5,52 @@ import bcrypt from "bcrypt";
 import User from "../models/User.js";
 import Jwt from 'jsonwebtoken';
 
-const Register = async (req,res) => {
-
-    const errors=validationResult(req);
-    if(errors.isEmpty()){
-        const {name, username,password,email}=req.body;
-
-        const salt= await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(password, salt);
-        
-        const userExist = await User.findOne({ $or: [{
-            email: email
-        }, {
-            username: username
-        }]});
-
-        if(userExist){
-            return res.json(jsonGenerate(StatusCode.UNPROCESSABLE_ENTITY, 
-                "User or Email already exists"))
-        }
-
-        try{
-            const result=await User.create({
-                name:name,
-                email:email,
-                password:hashPassword,
-                username:username
-            })
-
-            const token = Jwt.sign({userId: result._id}, JWT_TOKEN_SECRET);
-
-            res.json(jsonGenerate(StatusCode.SUCCESS,"Registration successfull", {userId: result._id, token: token}));
-
-        }catch (error){
-            console.log(error)
-        }
-
-
+const Register = async (req, res) => {
+    // Validate request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(StatusCode.VALIDATION_ERROR).json(
+            jsonGenerate(StatusCode.VALIDATION_ERROR, "Validation error", errors.mapped())
+        );
     }
 
+    const { name, username, password, email } = req.body;
 
-    res.json(jsonGenerate(StatusCode.VALIDATION_ERROR,"validation error",errors.mapped()));
-}
+    try {
+        // Check if user or email already exists
+        const userExist = await User.findOne({ $or: [{ email }, { username }] });
+        if (userExist) {
+            return res.status(StatusCode.UNPROCESSABLE_ENTITY).json(
+                jsonGenerate(StatusCode.UNPROCESSABLE_ENTITY, "User or Email already exists")
+            );
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        // Create new user
+        const result = await User.create({
+            name,
+            email,
+            password: hashPassword,
+            username
+        });
+
+        // Generate JWT token
+        const token = Jwt.sign({ userId: result._id }, JWT_TOKEN_SECRET);
+
+        // Send success response
+        return res.status(StatusCode.SUCCESS).json(
+            jsonGenerate(StatusCode.SUCCESS, "Registration successful", { userId: result._id, token })
+        );
+
+    } catch (error) {
+        console.error(error);
+        return res.status(StatusCode.INTERNAL_SERVER_ERROR).json(
+            jsonGenerate(StatusCode.INTERNAL_SERVER_ERROR, "Internal Server Error")
+        );
+    }
+};
 
 export default Register;
